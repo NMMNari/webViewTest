fdd73c2dfd4ed48e9cc352e2ca6aa5de84a66e74
NMMNari <89003800+NMMNari@users.noreply.github.com>
2025-07-27 00:53:43 +0900

    html外部化

--- Assets/Scripts/WebViewLauncher.cs
+++ Assets/Scripts/WebViewLauncher.cs
@@ -1,34 +1,48 @@
 using System.Collections;
-using System.IO;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class WebViewLauncher : MonoBehaviour
 {
+    /* 外部公開している HTML の RAW URL */
+    const string RemoteHtmlUrl = "https://raw.githubusercontent.com/NMMNari/html_testgame1/main/invaders_game.html";
+
     WebViewObject _webView;
 
     IEnumerator Start()
     {
-        yield return StreamingAssetsCopier.CopyFolderToPersistent("WebInvaders");
+        /* ① HTML をダウンロード */
+        string html = null;
+        using (UnityWebRequest www = UnityWebRequest.Get(RemoteHtmlUrl))
+        {
+            yield return www.SendWebRequest();
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError("[WebInvaders] HTML download failed: " + www.error);
+                yield break;   // 失敗時にローカルへフォールバックしたい場合はここで呼ぶ
+            }
+            html = www.downloadHandler.text;
+        }
 
+        /* ② WebView を生成 */
         _webView = gameObject.AddComponent<WebViewObject>();
-        _webView.Init(transparent: false, enableWKWebView: true,
-            ld: msg => Debug.Log("Loaded: " + msg));
-
+        _webView.Init(
+            transparent: false,
+            enableWKWebView: true,
+            ld: msg => Debug.Log("Loaded (external): " + msg)
+        );
         _webView.SetMargins(0, 0, 0, 0);
 
 #if UNITY_ANDROID && !UNITY_EDITOR
-        // 旧バージョン互換：RequestFocus がある時だけ呼ぶ
+        // 旧バージョンとの互換を考慮
         var m = _webView.GetType().GetMethod("RequestFocus");
         if (m != null) m.Invoke(_webView, null);
 #endif
 
-        string url =
-#if UNITY_EDITOR
-            Path.Combine(Application.streamingAssetsPath, "WebInvaders/invaders_game.html");
-#else
-            Path.Combine(Application.persistentDataPath,  "WebInvaders/invaders_game.html");
-#endif
-        _webView.LoadURL(new System.Uri(url).AbsoluteUri);
+        /* ③ 文字列で HTML を流し込む */
+        // 第2引数 baseUrl を渡しておくと、HTML 内の相対パス解決や LocalStorage が機能する
+        _webView.LoadHTML(html, RemoteHtmlUrl.Substring(0, RemoteHtmlUrl.LastIndexOf('/') + 1));
+
         _webView.SetVisibility(true);
         _webView.EvaluateJS("window.focus();");
     }
